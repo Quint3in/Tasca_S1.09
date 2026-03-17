77c63053b36bc31e6daf35c77bfa5aeefbdcd882
JoelQuintana <joelqn95@gmail.com>
2026-03-17 13:20:30 +0100

    feat(n2exercici1): Calculate date differences with Period and Duration

--- n2exercici1/src/main/java/org/example/Main.java
+++ n2exercici1/src/main/java/org/example/Main.java
@@ -1,17 +1,24 @@
 package org.example;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
+import java.time.*;
 
 public class Main {
     static void main() {
         LocalDate ld = LocalDate.now();
         LocalTime lt = LocalTime.now();
-        LocalDateTime ldt = LocalDateTime.now();
+        LocalDateTime ldt1 = LocalDateTime.now();
+
 
         System.out.println(ld.toString());
         System.out.println(lt.toString());
-        System.out.println(ldt.toString());
+        System.out.println(ldt1.toString());
+
+        LocalDateTime ldt2 = ldt1.plusDays(5).plusHours(10);
+
+        Period period1 = Period.between(ldt1.toLocalDate(), ldt2.toLocalDate());
+        Duration duration1 = Duration.between(ldt1, ldt2);
+        System.out.println("Diferencia en dias y horas");
+        System.out.println(period1.getDays());
+        System.out.println(duration1.toHours());
     }
 }
