32692c62c589caa90c951c57d5a410298c9aedc4
JoelQuintana <joelqn95@gmail.com>
2026-03-17 13:29:45 +0100

    feat(n2exercici1): Add DateTimeFormatter examples and output separators

--- n2exercici1/src/main/java/org/example/Main.java
+++ n2exercici1/src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.time.*;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
     public static void main(String[] args) {
@@ -17,12 +18,26 @@ public class Main {
 
         Period period1 = Period.between(ldt1.toLocalDate(), ldt2.toLocalDate());
         Duration duration1 = Duration.between(ldt1, ldt2);
+        System.out.println("---------------------------------");
         System.out.println("Diferencia en dias y horas");
         System.out.println(period1.getDays());
         System.out.println(duration1.toHours());
 
         ldt2 = ldt2.plusYears(5).plusMonths(1).minusYears(10).minusHours(20);
+        System.out.println("---------------------------------");
         System.out.println("Restar/Sumar a la fecha");
         System.out.println(ldt2.toString());
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        DateTimeFormatter formatter3 = DateTimeFormatter.ofPattern("EEE, dd MMM yyyy");
+        System.out.println("---------------------------------");
+        System.out.println("Fecha con distintos formatos");
+        System.out.println(ldt2.format(formatter));
+        System.out.println(ldt2.format(formatter2));
+        System.out.println(ldt2.format(formatter3));
+
+        System.out.println("---------------------------------");
+
     }
 }
