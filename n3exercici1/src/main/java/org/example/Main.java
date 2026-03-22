1b18632aa14d11f7e0e76e02b928fd292a564c5f
JoelQuintana <joelqn95@gmail.com>
2026-03-22 11:41:10 +0100

    feat(n3exercici1): add kill method on Person and update Main demo

--- n3exercici1/src/main/java/org/example/Main.java
+++ n3exercici1/src/main/java/org/example/Main.java
@@ -1,12 +1,14 @@
 package org.example;
 
 public class Main {
-    static void main() {
+    public static void main(String[] args) {
         System.out.println("Primera Instancia");
-        Person p = new Person("John", 25);
-        System.out.println(p.name());
-        System.out.println(p.age());
+        Person p1 = new Person("John", 25);
+        System.out.println(p1.name());
+        System.out.println(p1.age());
 
-
+        System.out.println("Método Personalizado");
+        Person p2 = new Person("Marcus", 23);
+        p2.kill(p1);
     }
 }
--- n3exercici1/src/main/java/org/example/Person.java
+++ n3exercici1/src/main/java/org/example/Person.java
@@ -1,4 +1,7 @@
 package org.example;
 
 public record Person(String name, int age) {
+    public void kill(Person victim) {
+        System.out.println( victim.name() + " is killed by " + name());
+    }
 }
