ca4a895f29816dd7093df3b5efa5add9e97129a0
JoelQuintana <joelqn95@gmail.com>
2026-03-22 11:45:50 +0100

    feat(n3exercici1): validate Person constructor and update Main examples

--- n3exercici1/src/main/java/org/example/Main.java
+++ n3exercici1/src/main/java/org/example/Main.java
@@ -1,14 +1,28 @@
 package org.example;
 
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Primera Instancia");
+        System.out.println("\nPrimera Instancia");
         Person p1 = new Person("John", 25);
         System.out.println(p1.name());
         System.out.println(p1.age());
 
-        System.out.println("Método Personalizado");
+        System.out.println("\nMétodo Personalizado");
         Person p2 = new Person("Marcus", 23);
         p2.kill(p1);
+
+        System.out.println("\nValidación Constructor");
+        try {
+            Person p3 = new Person("James", -1);
+        } catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+        }
+        try {
+            Person p4 = new Person("", 23);
+        } catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
--- n3exercici1/src/main/java/org/example/Person.java
+++ n3exercici1/src/main/java/org/example/Person.java
@@ -1,6 +1,15 @@
 package org.example;
 
 public record Person(String name, int age) {
+
+    public Person {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("name no puede ser vacío");
+        }
+        if (age < 0) {
+            throw new IllegalArgumentException("age no puede ser negativo");
+        }
+    }
     public void kill(Person victim) {
         System.out.println( victim.name() + " is killed by " + name());
     }
